d5aced82df9de661358c8fdc795c6da9dc4252e7
Enrique <enrique.gomez.electronico@gmail.com>
2024-02-11 19:48:51 +0100

    add header

--- emociones/src/App.js
+++ emociones/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+       <h1>Emociones</h1>
+      </header>
     </div>
   );
 }
