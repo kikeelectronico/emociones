d301c53af1b7673df7f8646f702aff33a924f7ac
Enrique <enrique.gomez.electronico@gmail.com>
2024-02-11 20:45:00 +0100

    selectable emotions

--- emociones/src/App.js
+++ emociones/src/App.js
@@ -8,6 +8,18 @@ function App() {
 
   const [selected, setSelected] = useState([])
 
+  const selectEmotion = (emotion_name) => {
+    let _selected = [...selected]
+    if (!_selected.includes(emotion_name)) {
+      _selected.push(emotion_name)
+    } else {
+      let index = _selected.indexOf(emotion_name)
+      _selected.splice(index, 1)
+    }
+
+    setSelected(_selected)
+  }
+
   return (
     <div className="App">
       <header className="header">
@@ -20,6 +32,8 @@ function App() {
               <Emotion 
                 emotion={emotion}
                 selected={selected.includes(emotion.name)}
+                key={emotion.name}
+                select={selectEmotion}
               />
             )
           })
--- emociones/src/components/Emotion.js
+++ emociones/src/components/Emotion.js
@@ -4,7 +4,10 @@ import "./emotion.css"
 export default function Emotion(props) {
 
   return (
-    <div className={"emotion-container" + (props.selected ? " emotion-selected" : "")}>
+    <div
+        className={"emotion-container" + (props.selected ? " emotion-selected" : "")}        
+        onClick={() => {props.select(props.emotion.name)}}
+    >
         <span className="emotion-name">
             {props.emotion.name}
         </span>
