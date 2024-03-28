c494a5b6d78129d494a483b19fefa46a17aa4aac
Enrique <enrique.gomez.electronico@gmail.com>
2024-03-28 21:59:50 +0100

    add buttons in modal

--- emociones/src/components/Modal.js
+++ emociones/src/components/Modal.js
@@ -1,4 +1,8 @@
 import React from "react";
+import IconButton from '@mui/material/IconButton';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+
 import "./modal.css"
 
 export default function Modal(props) {
@@ -17,6 +21,23 @@ export default function Modal(props) {
                             <p>
                                 Los datos se guardan solo en tu dispositivo por lo que solo tu puedes acceder a ellos.
                             </p>
+                            <p>
+                                Si lo deseas puedes usar los siguientes botones para descargar un archivo con tus datos o importarlos para restaurarlos.
+                            </p>
+                        </div>
+                        <div className="modal-buttons">
+                            <IconButton
+                                aria-label="Descargar datos"
+                                onClick={() => {}}
+                            >
+                                <FileDownloadOutlinedIcon  className="iconButton"/>
+                            </IconButton>
+                            <IconButton
+                                aria-label="Restaurar datos"
+                                onClick={() => {}}
+                            >
+                                <FileUploadOutlinedIcon  className="iconButton"/>
+                            </IconButton>
                         </div>
                     </div>
                 </div>
